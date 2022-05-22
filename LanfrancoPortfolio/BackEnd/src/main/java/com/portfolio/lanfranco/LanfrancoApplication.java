1ac749a329c992f62c99d9e4f8135510251889f9
DarioLanfranco <dariolanfrancoruffener@gmail.com>
2022-05-22 17:51:08 -0300

    intro al backend

---
+++ LanfrancoPortfolio/BackEnd/src/main/java/com/portfolio/lanfranco/LanfrancoApplication.java
@@ -0,0 +1,14 @@
+package com.portfolio.lanfranco;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class LanfrancoApplication {
+//
+	public static void main(String[] args) {
+//		SpringApplication.run(LanfrancoApplication.class, args);
+	}
+
+}
+ 
---
+++ LanfrancoPortfolio/BackEnd/src/test/java/com/portfolio/lanfranco/LanfrancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.lanfranco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanfrancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
